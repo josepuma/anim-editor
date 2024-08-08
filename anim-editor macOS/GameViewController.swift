29dbffa39ab8938c79ff11b997c5c0686d979cc1
José Puma <45923235+josepuma@users.noreply.github.com>
2024-08-08 16:38:36 -0400

    Initial Commit

---
+++ anim-editor macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  anim-editor macOS
+//
+//  Created by José Puma on 08-08-24.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
